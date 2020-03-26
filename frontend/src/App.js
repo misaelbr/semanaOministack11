2c4fa71f4e93ff657f6686aa44be4f23100a0a01
Misael Bandeira Silveira <misael.bandeira@gmail.com>
2020-03-25 21:08:48 -0300

    Ajustes na app e movimentação da pasta asset

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from './Header';
 
 function App() {
-  const counter = 0;
+  const [ counter, setCounter ] = useState(0);
+  // array [valor, funcaoDeAtualizacao]
+
+  function increment() {
+    setCounter( counter + 1 )
+  }
   return (
     <div>
       <Header>Contador: {counter}</Header>
